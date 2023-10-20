d8097f15c4d537eb28fca9ef60237a2741a7ccf8
Alfred <alfred.garcia@ucb.edu.bo>
2023-10-20 13:18:09 -0400

    minmax function and positions on the board.

---
+++ src/adversarial_search.py
@@ -0,0 +1,96 @@
+from moves_helper import MoveHelper
+from copy import deepcopy
+from settings import Settings
+
+class AdversarialSearch:
+    computer = None
+    beta = None
+    alpha = None
+
+    @staticmethod
+    def min_max_with_depth(current_player, board, player_enemy, unique_values):
+        AdversarialSearch.beta = Settings.highest_value
+        AdversarialSearch.alpha = Settings.lowest_value
+
+        AdversarialSearch.computer = deepcopy(current_player)
+        p_moves = MoveHelper.get_possible_moves(AdversarialSearch.computer, deepcopy(board))
+
+        if len(p_moves) > 1:
+            heuristic_values = []
+            for p_move in deepcopy(unique_values):
+                new_board = deepcopy(board)
+                new_move = deepcopy(p_move)
+                new_enemy = deepcopy(player_enemy)
+                adv_search = AdversarialSearch._max_value(0, new_board, new_move, deepcopy(AdversarialSearch.computer ), new_enemy)
+                heuristic_values.append(adv_search)
+            return heuristic_values.index(min(heuristic_values)) + 1
+        elif len(p_moves) == 1:
+            return 1
+        else:
+            return None
+
+    @staticmethod
+    def _min_value(depth, board, possible_moves, current_player, player_enemy):
+        # print("Board recibido------------------------------------------")
+        # board.display(0, 0, current_player, player_enemy)
+
+        MoveHelper.apply_move(board, current_player, player_enemy, possible_moves)
+        # print("Board final------------------------------------------")
+        # board.display(0, 0, current_player, player_enemy)
+
+        if AdversarialSearch._cut_off(depth):
+            computer = current_player if current_player.token == AdversarialSearch.computer.token else player_enemy
+            return - AdversarialSearch._eval(board, computer, possible_moves)
+        value = Settings.highest_value
+        p_moves = MoveHelper.get_possible_moves(player_enemy, board)
+
+        unique_opt, _ = MoveHelper.get_unique_final_pos(p_moves)
+        for p_move in unique_opt:
+            # print("profundidad",depth,"---------------------------------------------")
+            copied_enemy = deepcopy(player_enemy)
+            copied_current_player = deepcopy(current_player)
+            new_board = deepcopy(board)
+            new_possible_moves = deepcopy(p_move)
+            value = min(value, AdversarialSearch._max_value(depth + 1, new_board, new_possible_moves, copied_enemy, copied_current_player))
+            if value <= AdversarialSearch.alpha:
+                return value
+            AdversarialSearch.beta = min(AdversarialSearch.beta, value)
+        return value
+
+    @staticmethod
+    def _max_value(depth, board, possible_moves, current_player, player_enemy):
+        # print("Board recibido------------------------------------------")
+        # board.display(0, 0, current_player, player_enemy)
+
+        MoveHelper.apply_move(board, current_player, player_enemy, possible_moves)
+        # print("Board final------------------------------------------")
+        # board.display(0, 0, current_player, player_enemy)
+
+        if AdversarialSearch._cut_off(depth):
+            computer = current_player if current_player.token == AdversarialSearch.computer.token else player_enemy
+            return - AdversarialSearch._eval(board, computer,possible_moves)
+
+        value = Settings.lowest_value
+        p_moves = MoveHelper.get_possible_moves(player_enemy, board )
+
+        unique_opt, _ = deepcopy(MoveHelper.get_unique_final_pos(p_moves))
+
+        for p_move in unique_opt:
+            # print("profundidad",depth,"---------------------------------------------")
+            new_enemy = deepcopy(player_enemy)
+            new_c_player = deepcopy(current_player)
+            new_board = deepcopy(board)
+            new_possible_moves = deepcopy(p_move)
+            value = max(value, AdversarialSearch._min_value(depth + 1, new_board, new_possible_moves, new_enemy, new_c_player))
+            if value >= AdversarialSearch.beta:
+                return value
+            AdversarialSearch.alpha = max(AdversarialSearch.alpha, value)
+        return value
+
+    @staticmethod
+    def _eval(state, computer, possible_moves):
+        return Settings.heuristic(state, computer, possible_moves)
+
+    @staticmethod
+    def _cut_off(depth):
+        return depth == Settings.max_depth
